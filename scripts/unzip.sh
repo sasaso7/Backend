7f255afb1ef0b55ac1f4bc562c484bb33b12e4d5
Philip Pedersen <philip.pedersen@twoday.com>
2024-07-24 13:39:43 +0200

    Added latest changes to pipeline

---
+++ scripts/unzip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the destination directory
+cd /var/www/app/
+
+# Unzip the .zip file
+unzip my-application.zip
+
+# Optionally, remove the .zip file after extraction
+rm my-application.zip
